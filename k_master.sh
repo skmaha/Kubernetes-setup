68649c2c4268eebb63f36068a488680b00a1d238
M Sunil Kumar <10656726@INFCHSZA5587.lntinfotech.com>
2020-10-28 07:51:35 +0100

    update scripts

--- k_master.sh
+++ k_master.sh
@@ -12,7 +12,12 @@ chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Deploy Calico network
 echo "[TASK 3] Deploy Calico network"
-su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml"
+su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml" 2>/dev/null
+
+# Deploy Kubernetes-dashboard
+echo "[Task 4] Deploy Kubernetes-dashboard & dashboard-service account"
+su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/msunilkumar/Kubernetes-setup/master/dashboard.yaml" 2>/dev/null
+su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/msunilkumar/Kubernetes-setup/master/sa-dashbord.yaml" 2>/dev/null
 
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
